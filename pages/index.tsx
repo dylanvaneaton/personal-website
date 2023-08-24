7c62278e6f344880ef7c48d6a38c287a860be53d
[Dylan Van Eaton] <[dylanvaneaton@gmail.com]>
2023-08-24 15:54:32 -0400

    added intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,30 @@ import { NextPage } from "next";
 import { Button, Layout, Menu } from "antd";
 import React from "react";
 import type { MenuProps } from "antd";
-
-
+import Image from "next/image";
 
 const HomePage: NextPage = () => {
-  return(
-    <>
-    <h1 className="text-4xl text-white font-extrabold">The coolest site.</h1>
-    <p className="text-fuchsia-300 text-xl font-bold">The text has disappeared and it was mad dumb and stupid anyway.</p>
-    </>
-  )
+  return (
+    <div className="flex flex-col bg-lime-700 w-full pr-24">
+      <h1 className="italic text-2xl font-bold w-full text-right pr-6">
+        Information Technology and Cybersecurity
+      </h1>
+
+      <div className="flex flex-row justify-end w-full">
+        <h1 className="italic text-7xl font-bold whitespace-pre-wrap self-end pb-4 pr-16">
+          {"Dylan\nVan\nEaton"}
+        </h1>
+
+        <Image
+          src="/assets/dylanstanding.png"
+          alt="Dylan Picture"
+          height={538}
+          width={248}
+          className="pr-2"
+        ></Image>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
